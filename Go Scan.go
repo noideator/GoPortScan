d0f514e874a6e02af1d0e37e707e1e27ad51228b
noideator <55750306+noideator@users.noreply.github.com>
2023-05-12 00:53:39 +0200

    Add files via upload

---
+++ Go Scan.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+func scanPort(host string, port int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	target := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.DialTimeout("tcp", target, 2*time.Second)
+	if err != nil {
+		fmt.Printf("Port %d closed\n", port)
+		return
+	}
+	conn.Close()
+	fmt.Printf("Port %d open\n", port)
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the host to scan: ")
+	host, _ := reader.ReadString('\n')
+	host = strings.TrimSpace(host)
+
+	fmt.Printf("Scanning ports on host: %s\n", host)
+
+	var wg sync.WaitGroup
+
+	for port := 1; port <= 65535; port++ {
+		wg.Add(1)
+		go scanPort(host, port, &wg)
+	}
+
+	wg.Wait()
+}
